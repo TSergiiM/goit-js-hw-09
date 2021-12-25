382ee7b88476f1468f1d933d61d1e80ec0b6c4b0
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-25 23:36:35 +0200

    adds-refs

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,7 @@
+refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+refs.stop.setAttribute('disabled', 'true');
+let intervalId = null;
 
