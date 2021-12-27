54f3ad60d3408d33d59a2e5d1aa93623ee1448e9
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-27 22:21:47 +0200

    adds-refs-and-variables

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,22 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix';
+
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  timer: document.querySelector('.timer'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+
+refs.start.addEventListener('click', () => {
+  interval();
+  refs.start.disabled = true;
+});
+
+let deltaTime;
+let selectedDate = null;
+refs.start.disabled = true;
+
