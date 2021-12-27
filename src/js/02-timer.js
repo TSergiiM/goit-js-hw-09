c50e56797c12561fbcf2c13c70d6f9844656695b
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-27 22:23:39 +0200

    add-func-interval

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -42,3 +42,22 @@ flatpickr('#datetime-picker', options);
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
+// запускає таймер, виводить таймер на екран, зупиняє таймер при 00:00:00:00
+function interval() {
+  const timerId = setInterval(() => {
+    const currentTime = new Date();
+    deltaTime = selectedDate - currentTime;
+    // console.log('Current:', currentTime);
+    // console.log('Start:', selectedDate);
+    // console.log('Delta:', deltaTime);
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    // console.log(`${days}:${hours}:${minutes}:${seconds}`);
+    refs.days.textContent = addLeadingZero(days);
+    refs.hours.textContent = addLeadingZero(hours);
+    refs.minutes.textContent = addLeadingZero(minutes);
+    refs.seconds.textContent = addLeadingZero(seconds);
+    if (deltaTime < 1000) {
+      clearInterval(timerId);
+    }
+  }, 1000);
+}
