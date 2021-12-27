6a30ac003ec3635072ffc9eb773fad2bd82e96eb
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-27 22:24:07 +0200

    add-func-convert

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -61,3 +61,22 @@ function interval() {
     }
   }, 1000);
 }
+// конвертує мілісекунди в дні, години, хвилини, секунди
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
