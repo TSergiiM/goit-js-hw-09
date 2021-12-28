3746a3b0bcb9cc6580852477a1ddc2331b66965a
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-29 01:42:13 +0200

    finish-task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  let delays = Number(delay.value);
+  let steps = Number(step.value);
+  let amounts = Number(amount.value);
+  let position = 1;
+  // от як без цикла зробити?
+  for (position; position <= amounts; position += 1) {
+    createPromise(position, delays)
+      .then(({ position, delays }) => {
+        setTimeout(() => {
+          // console.log(`Fulfilled promise ${position} in ${delay}ms`);
+          Notify.success(`Fulfilled promise ${position} in ${delays}ms`);
+        }, delays);
+      })
+      .catch(({ position, delays }) => {
+        setTimeout(() => {
+          // console.log(`Rejected promise ${position} in ${delay}ms`);
+          Notify.failure(`Rejected promise ${position} in ${delays}ms`);
+        }, delays);
+      });
+    delays += steps;
   }
+}
+
+function createPromise(position, delays) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delays }); // Fulfill
+    } else {
+      reject({ position, delays }); // Reject
+    }
+  });
 }
